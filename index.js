1fdd111082ff79c06d0800a79f3e2f13e8f8200a
poojak31 <pooja.kolape@qliktag.com>
2020-02-20 11:02:33 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,48 +1,52 @@
-// Import stylesheets
-// import './style.css';
+// // Import stylesheets
+// // import './style.css';
 
 
 
-      function opration()
-      {
-       var no1=parseInt(document.getElementById('no1').value);
-       var no2=parseInt(document.getElementById('no2').value);
-       var oper=document.getElementById('operator').value;
-    
-    if(document.form1.no1.value == "" || document.form1.no1.value == null)
-    {
-            alert("enter no1 to calculate");
-    }
-    else if(document.form1.no2.value == "" || document.form1.no2.value == null)
-    {
-        alert("enter no2 to calculate");
-    }
+//       function opration()
+//       {
+//        var no1=parseInt(document.getElementById('no1').value);
+//        var no2=parseInt(document.getElementById('no2').value);
+//        var oper=document.getElementById('operator').value;
+//       var result=0;
+//     if(document.form1.no1.value == "" || document.form1.no1.value == null)
+//     {
+//             alert("enter no1 to calculate");
+//     }
+//     else if(document.form1.no2.value == "" || document.form1.no2.value == null)
+//     {
+//         alert("enter no2 to calculate");
+//     }
 
-    if(oper=="add")
-    {
-      document.getElementById('result').value =no1+no2;
-    }
-    if(oper=="subt")
-    {
-      document.getElementById('result').value = no1-no2;
-    }
-    if(oper=="mult")
-    {
-     document.getElementById('result').value = no1*no2;
-    }
-    if(oper=="div")
-    {
-      if(no2=='0')
-      {
-           alert('It is invalid number,please enter valid number');
-      }
-      else
-      {
-       document.getElementById('result').value = no1/no2;
-    }
-    }
+//     if(oper=="add")
+//     {
+//       result=document.getElementById('result').value =no1+no2;
+//          console.log("The addition of two numbers = " + result);
+//     }
+//     if(oper=="subt")
+//     {
+//       result=document.getElementById('result').value = no1-no2;
+//        console.log("The subtraction of two numbers = " + result);
+//     }
+//     if(oper=="mult")
+//     {
+//     result= document.getElementById('result').value = no1*no2
+//      console.log("The multiplication of two numbers = " + result);
+//     }
+//     if(oper=="div")
+//     {
+//       if(no2=='0')
+//       {
+//            alert('It is invalid number,please enter valid number');
+//       }
+//       else
+//       {
+//        result=document.getElementById('result').value = no1/no2;
+//        console.log("The division of two numbers = " + result);
+//     }
+//     }
       
-}
+// }
 
    
 
